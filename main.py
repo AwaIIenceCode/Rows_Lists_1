a10259244fa97d0bab9beeb82033645bfb7f7906
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-03 23:55:15 +0200

    Innitial commit for my test

--- main.py
+++
@@ -1,9 +0,0 @@
-def replace_word_in_string():
-    user_string = input('Enter the strings: ')
-    found_word = input('Enter the found word: ')
-    insert_word = input('Enter the insert word: ')
-
-    result_string = user_string.replace(found_word, insert_word)
-    print(result_string)
-
-replace_word_in_string()
