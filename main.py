033fe7199a7766ab06c9f736493b7c9355e983ad
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-04 01:08:52 +0200

    ex_2 from tasks 2 from hm is done

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-# В списке целых, заполненном случайными числами, определить минимальный и максимальный элементы,
-# сосчитать количество отрицательных элементов, сосчитать количество положительных элементов,
-# сосчитать количество нулей. Результаты выводятся на экран.
+magenta = '\033[35m'
+reset = '\033[0m'
 
 def calculating_numbers():
-    numbers_list = [3, 74, 0, 12, -4, 8, 0, -7, 5, 12, 34, 87, -7, 0, 0, 6, 0, 32, 76, 88, -5, 0, -0,75]
+    numbers_list = [3, 74, 0, 12, -4, 8, 0, -7, 5, 12, 34, 87, -7, 0, 0, 6, 0, 32, 76, 88, -5, 0, -0.75]
     count_of_negative_num = 0
     count_of_positive_num = 0
     count_of_zero = 0
@@ -18,13 +17,14 @@ def calculating_numbers():
         else:
             count_of_zero += 1
 
-    for number in numbers_list:
+
         if number < my_min_number:
             my_min_number = number
 
         elif number > my_max_number:
             my_max_number = number
 
-    print(f'Min: {my_min_number}, Max: {my_max_number}, Zero: {count_of_zero}, Positive: {count_of_positive_num}, Negative: {count_of_negative_num}')
+    print(f'Your min: {magenta}{my_min_number}{reset}\nYour max: {magenta}{my_max_number}{reset}\nNumber of zeroes: {magenta}{count_of_zero}{reset}')
+    print(f'Number of pozitive: {magenta}{count_of_positive_num}{reset}\nNumber of negative: {magenta}{count_of_negative_num}{reset}')
 
 calculating_numbers()
